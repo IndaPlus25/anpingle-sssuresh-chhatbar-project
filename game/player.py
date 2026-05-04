b7c81b197fea6553227da802002d29021fc09930
Arnav Pingle <anpingle@kth.se>
2026-05-04 18:02:11 +0200

    finished first impl of top-down

--- game/player.py
+++ game/player.py
@@ -5,8 +5,24 @@ class Player:
         self.portfolio = {}
         self.x = 300
         self.y = 200
+        self.direction = "south"
+        self.is_moving = False
 
-    def move(self, dx):
-        self.x += dx
-        self.x = max(0, min(self.x, 600 - 40))
-
+    def move(self, dx, dy, max_w, max_h):
+        if dx != 0 or dy != 0:
+            self.is_moving = True
+            self.x += dx
+            self.y += dy
+            
+            self.x = max(0, min(self.x, max_w - 40))
+            self.y = max(0, min(self.y, max_h - 40))
+            
+            
+            dirs = []
+            if dy < 0: dirs.append("north")
+            elif dy > 0: dirs.append("south")
+            if dx > 0: dirs.append("east")
+            elif dx < 0: dirs.append("west")
+            self.direction = "".join(dirs)
+        else:
+            self.is_moving = False
